e1ea372a448adce37e328c32c6dfe07ee74c8e2d
eroell <eljas.roellin@ikmail.com>
2024-04-26 17:56:17 +0200

    norm example more concise and ehrapyfied

--- utils.py
+++ utils.py
@@ -40,3 +40,61 @@ def hist_plot_num_medication(adata_to_plot, title, has_missing):
     plt.title(title)
     plt.legend()
     return mean_value, median_value, std_value
+
+
+
+def plot_normalization(adata, adata_scaled_together, adata_scaled_separate, batch_key="age_group", var_of_interest="num_medications"):
+    """Plot histogram of original data, jointly normalized, and split normalization (by batch_key variable)."""
+
+    fig, axs = plt.subplots(1, 3, figsize=(21, 5))
+
+    # plot raw adata
+    for group in adata.obs[batch_key].unique():
+        adata_group = adata[adata.obs[batch_key] == group, var_of_interest]
+        axs[0].hist(
+            adata_group.X,
+            bins=10,
+            alpha=0.7,
+            label=group,
+            orientation="horizontal",
+            weights = np.ones(len(adata_group)) / len(adata_group)
+        )
+        axs[0].set_ylabel(var_of_interest)
+        axs[0].set_xlabel("% of visits per group")
+        axs[0].legend(title=batch_key)
+        axs[0].set_title(f"Original Distributions of {var_of_interest}")
+
+    # plot together normalized data
+    for group in adata_scaled_together.obs[batch_key].unique():
+        adata_group = adata_scaled_together[adata_scaled_together.obs[batch_key] == group, var_of_interest]
+        axs[1].hist(
+            adata_group.X,
+            bins=10,
+            alpha=0.7,
+            label=group,
+            orientation="horizontal",
+            weights = np.ones(len(adata_group)) / len(adata_group)
+        )
+        axs[1].set_ylabel(var_of_interest)
+        axs[1].set_xlabel("% of visits per group")
+        axs[1].legend(title=batch_key)
+        axs[1].set_title(f"Overall normalization of {var_of_interest}")
+
+    # plot separately normalized data
+    for group in adata_scaled_separate.obs[batch_key].unique():
+        adata_group = adata_scaled_separate[adata_scaled_separate.obs[batch_key] == group, var_of_interest]
+        axs[2].hist(
+            adata_group.X,
+            bins=10,
+            alpha=0.7,
+            label=group,
+            orientation="horizontal",
+            weights = np.ones(len(adata_group)) / len(adata_group)
+        )
+        axs[2].set_ylabel(var_of_interest)
+        axs[2].set_xlabel("% of visits per group")
+        axs[2].legend(title=batch_key)
+        axs[2].set_title(f"Separate normalization of {var_of_interest}")
+
+    plt.tight_layout()
+    plt.show()
