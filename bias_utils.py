78d98864d1e2bb0c807677c61fb82882c7545195
eroell <eljas.roellin@ikmail.com>
2024-04-29 12:50:23 +0200

    simplify and slightly tidy up

--- bias_utils.py
+++ bias_utils.py
@@ -6,9 +6,8 @@ import matplotlib.pyplot as plt
 def plot_hist_num_medication(adata_to_plot, title, has_missing):
     """Plot histogram of number of medications per visit."""
     num_medications = adata_to_plot.X[:, adata_to_plot.var_names == "num_medications"]
-    mean_value = np.nanmean(num_medications)
-    median_value = np.nanmedian(num_medications)
-    std_value = np.nanstd(num_medications)
+    mean_value = np.nanmean(num_medications.astype(np.float64))
+    std_value = np.nanstd(num_medications.astype(np.float64))
 
     plt.hist(num_medications, bins=10, alpha=0.5)
 
@@ -20,13 +19,7 @@ def plot_hist_num_medication(adata_to_plot, title, has_missing):
         linewidth=1,
         label=f"Mean{missing_hint}: {mean_value:.2f}",
     )
-    plt.axvline(
-        median_value,
-        color="g",
-        linestyle="dashed",
-        linewidth=1,
-        label=f"Median{missing_hint}: {median_value:.2f}",
-    )
+
     plt.axvline(
         mean_value + std_value,
         color="b",
@@ -39,7 +32,7 @@ def plot_hist_num_medication(adata_to_plot, title, has_missing):
     plt.ylabel("Num. Visits")
     plt.title(title)
     plt.legend()
-    return mean_value, median_value, std_value
+    return mean_value, std_value
 
 
 
