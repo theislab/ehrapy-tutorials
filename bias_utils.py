415a24a33f45e2e1dd680ab5906365a11e858e94
eroell <eljas.roellin@ikmail.com>
2024-05-08 09:58:01 +0200

    prettier plots, enhanced text

--- bias_utils.py
+++ bias_utils.py
@@ -1,49 +1,91 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
 
+def distributions_plot(labels, adatas, var_of_interest):
+    """Creates a raincloud-plot inspired distribution plot for the `var_of_interest` variable within given adatas."""
 
+    colors = sns.color_palette("tab10")[:len(adatas)]
 
-def plot_hist_num_medication(adata_to_plot, title, has_missing):
-    """Plot histogram of number of medications per visit."""
-    num_medications = adata_to_plot.X[:, adata_to_plot.var_names == "num_medications"]
-    mean_value = np.nanmean(num_medications.astype(np.float64))
-    std_value = np.nanstd(num_medications.astype(np.float64))
+    height_ratios = [10] + [1] * len(adatas)
 
-    plt.hist(num_medications, bins=10, alpha=0.5)
-
-    missing_hint = " (nan ignored)" if has_missing else ""
-    plt.axvline(
-        mean_value,
-        color="r",
-        linestyle="dashed",
-        linewidth=1,
-        label=f"Mean{missing_hint}: {mean_value:.2f}",
+    # Create the subplots with specified height ratios
+    fig, axes = plt.subplots(
+        nrows=1 + len(adatas),
+        figsize=(6, 1.5 * len(adatas)),
+        gridspec_kw={'height_ratios': height_ratios},
+        sharex=True,
     )
+    fig.subplots_adjust(hspace=0)
 
-    plt.axvline(
-        mean_value + std_value,
-        color="b",
-        linestyle="dashed",
-        linewidth=1,
-        label=f"Stdev{missing_hint}: {std_value:.2f}",
-    )
-    plt.axvline(mean_value - std_value, color="b", linestyle="dashed", linewidth=1)
-    plt.xlabel("Num. Medications")
-    plt.ylabel("Num. Visits")
-    plt.title(title)
-    plt.legend()
-    return mean_value, std_value
+    for i, (color, label, data) in enumerate(zip(colors, labels, adatas)):
+        statistics = {
+            "Mean": np.nanmean(data[:, data.var_names == var_of_interest].X.astype(np.float64)),
+            "Stdev": np.nanstd(data[:, data.var_names == var_of_interest].X.astype(np.float64))
+            }
+        stat_string = f"${round(statistics['Mean'].item(), 2)} \pm {round(statistics['Stdev'].item(), 2)}$"
+        
+        sns.kdeplot(
+            x=data[:, data.var_names==var_of_interest].X.flatten(),
+            color=color,
+            ax=axes[0],
+            label=f"{label}: {stat_string}",
+        )
+        sns.boxplot(
+            x=data[:, data.var_names==var_of_interest].X.flatten(),
+            orient="h", 
+            color=color,
+            ax=axes[i + 1],
+            width=0.25,
+            fliersize=0.3
+        )
+        sns.rugplot(
+            x=data[:, data.var_names==var_of_interest].X.flatten(),
+            color=color,
+            alpha=0.1,
+            ax=axes[i + 1],
+            height=0.2
+        )
+        
+        axes[i+1].spines["right"].set_visible(False)
+        axes[i+1].spines["left"].set_visible(False)
+        axes[i+1].spines["bottom"].set_visible(False)
+        axes[i+1].set_yticks([])
+
+        if i > 0:
+            axes[i+1].spines["top"].set_visible(False)
+        
+    fig.suptitle(var_of_interest, )
+    axes[0].legend()
 
 
+def grouped_barplot(df):
+    """Creates grouped barplots for a Fairlearn MetricFrame.bygroups dataframe"""
+    df_long = pd.melt(df.reset_index(), id_vars=['race'], var_name='metric', value_name='value')
 
-def plot_hist_normalization(adata, adata_scaled_together, adata_scaled_separate, batch_key="age_group", var_of_interest="num_medications"):
-    """Plot histogram of original data, jointly normalized, and split normalization (by batch_key variable)."""
+    # cohorttracker color palette
+    colors = sns.color_palette("colorblind", n_colors=14)
+
+    # colors from cohorttracker
+    palette = [colors[0], colors[2], colors[3], colors[5]]
+
+    fig, ax = plt.subplots()
+    bar = sns.barplot(data=df_long, x="metric", y="value", hue="race", palette = palette)
+    ax.legend(title="Race")
+    ax.set_ylabel("Value")
+    ax.set_xlabel("Metric")
+    ax.set_xticklabels(labels=["Selection Rate", "False Negative Rate", "Balanced Accuracy"])
+
+
+def plot_hist_normalization(adata, adata_scaled_together, adata_scaled_separate, group_key="age_group", var_of_interest="num_medications"):
+    """Plot histogram of original data, jointly normalized, and split normalization (by group_key variable)."""
 
     fig, axs = plt.subplots(1, 3, figsize=(21, 5))
 
     # plot raw adata
-    for group in adata.obs[batch_key].unique():
-        adata_group = adata[adata.obs[batch_key] == group, var_of_interest]
+    for group in adata.obs[group_key].unique():
+        adata_group = adata[adata.obs[group_key] == group, var_of_interest]
         axs[0].hist(
             adata_group.X,
             bins=10,
@@ -54,12 +96,12 @@ def plot_hist_normalization(adata, adata_scaled_together, adata_scaled_separate,
         )
         axs[0].set_ylabel(var_of_interest)
         axs[0].set_xlabel("% of visits per group")
-        axs[0].legend(title=batch_key)
+        axs[0].legend(title=group_key)
         axs[0].set_title(f"Original Distributions of {var_of_interest}")
 
     # plot together normalized data
-    for group in adata_scaled_together.obs[batch_key].unique():
-        adata_group = adata_scaled_together[adata_scaled_together.obs[batch_key] == group, var_of_interest]
+    for group in adata_scaled_together.obs[group_key].unique():
+        adata_group = adata_scaled_together[adata_scaled_together.obs[group_key] == group, var_of_interest]
         axs[1].hist(
             adata_group.X,
             bins=10,
@@ -70,12 +112,12 @@ def plot_hist_normalization(adata, adata_scaled_together, adata_scaled_separate,
         )
         axs[1].set_ylabel(var_of_interest)
         axs[1].set_xlabel("% of visits per group")
-        axs[1].legend(title=batch_key)
+        axs[1].legend(title=group_key)
         axs[1].set_title(f"Overall normalization of {var_of_interest}")
 
     # plot separately normalized data
-    for group in adata_scaled_separate.obs[batch_key].unique():
-        adata_group = adata_scaled_separate[adata_scaled_separate.obs[batch_key] == group, var_of_interest]
+    for group in adata_scaled_separate.obs[group_key].unique():
+        adata_group = adata_scaled_separate[adata_scaled_separate.obs[group_key] == group, var_of_interest]
         axs[2].hist(
             adata_group.X,
             bins=10,
@@ -86,7 +128,7 @@ def plot_hist_normalization(adata, adata_scaled_together, adata_scaled_separate,
         )
         axs[2].set_ylabel(var_of_interest)
         axs[2].set_xlabel("% of visits per group")
-        axs[2].legend(title=batch_key)
+        axs[2].legend(title=group_key)
         axs[2].set_title(f"Separate normalization of {var_of_interest}")
 
     plt.tight_layout()
