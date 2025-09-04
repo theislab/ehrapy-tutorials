5536e5f9c76dbd972bc0e65593783f32e5fe9577
Lukas Heumos <lukas.heumos@posteo.net>
2025-09-04 18:18:10 +0200

    upgrade pre-commit
    Signed-off-by: Lukas Heumos <lukas.heumos@posteo.net>

--- bias_utils.py
+++ bias_utils.py
@@ -1,12 +1,11 @@
-import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
+import numpy as np
 import pandas as pd
+import seaborn as sns
 
 
 def distributions_plot(labels, adatas, var_of_interest):
     """Creates a raincloud-plot inspired distribution plot for the `var_of_interest` variable within given adatas."""
-
     colors = sns.color_palette("tab10")[: len(adatas)]
 
     height_ratios = [10] + [1] * len(adatas)
@@ -22,14 +21,10 @@ def distributions_plot(labels, adatas, var_of_interest):
 
     for i, (color, label, data) in enumerate(zip(colors, labels, adatas)):
         statistics = {
-            "Mean": np.nanmean(
-                data[:, data.var_names == var_of_interest].X.astype(np.float64)
-            ),
-            "Stdev": np.nanstd(
-                data[:, data.var_names == var_of_interest].X.astype(np.float64)
-            ),
+            "Mean": np.nanmean(data[:, data.var_names == var_of_interest].X.astype(np.float64)),
+            "Stdev": np.nanstd(data[:, data.var_names == var_of_interest].X.astype(np.float64)),
         }
-        stat_string = f"${round(statistics['Mean'].item(), 2)} \pm {round(statistics['Stdev'].item(), 2)}$"
+        stat_string = rf"${round(statistics['Mean'].item(), 2)} \pm {round(statistics['Stdev'].item(), 2)}$"
 
         sns.kdeplot(
             x=data[:, data.var_names == var_of_interest].X.flatten(),
@@ -66,10 +61,8 @@ def distributions_plot(labels, adatas, var_of_interest):
 
 
 def grouped_barplot(df):
-    """Creates grouped barplots for a Fairlearn MetricFrame.bygroups dataframe"""
-    df_long = pd.melt(
-        df.reset_index(), id_vars=["race"], var_name="metric", value_name="value"
-    )
+    """Creates grouped barplots for a Fairlearn MetricFrame.bygroups dataframe."""
+    df_long = pd.melt(df.reset_index(), id_vars=["race"], var_name="metric", value_name="value")
 
     # cohorttracker color palette
     colors = sns.color_palette("colorblind", n_colors=14)
@@ -78,14 +71,12 @@ def grouped_barplot(df):
     palette = [colors[0], colors[2], colors[3], colors[5]]
 
     fig, ax = plt.subplots()
-    bar = sns.barplot(data=df_long, x="metric", y="value", hue="race", palette=palette)
+    sns.barplot(data=df_long, x="metric", y="value", hue="race", palette=palette)
     ax.legend(title="Race", bbox_to_anchor=(1.05, 1))
 
     ax.set_ylabel("Value")
     ax.set_xlabel("Metric")
-    ax.set_xticklabels(
-        labels=["Selection Rate", "False Negative Rate", "Balanced Accuracy"]
-    )
+    ax.set_xticklabels(labels=["Selection Rate", "False Negative Rate", "Balanced Accuracy"])
 
 
 def plot_hist_normalization(
@@ -96,7 +87,6 @@ def plot_hist_normalization(
     var_of_interest="num_medications",
 ):
     """Plot histogram of original data, jointly normalized, and split normalization (by group_key variable)."""
-
     fig, axs = plt.subplots(1, 3, figsize=(21, 5))
 
     # plot raw adata
@@ -117,9 +107,7 @@ def plot_hist_normalization(
 
     # plot together normalized data
     for group in adata_scaled_together.obs[group_key].unique():
-        adata_group = adata_scaled_together[
-            adata_scaled_together.obs[group_key] == group, var_of_interest
-        ]
+        adata_group = adata_scaled_together[adata_scaled_together.obs[group_key] == group, var_of_interest]
         axs[1].hist(
             adata_group.X,
             bins=10,
@@ -135,9 +123,7 @@ def plot_hist_normalization(
 
     # plot separately normalized data
     for group in adata_scaled_separate.obs[group_key].unique():
-        adata_group = adata_scaled_separate[
-            adata_scaled_separate.obs[group_key] == group, var_of_interest
-        ]
+        adata_group = adata_scaled_separate[adata_scaled_separate.obs[group_key] == group, var_of_interest]
         axs[2].hist(
             adata_group.X,
             bins=10,
